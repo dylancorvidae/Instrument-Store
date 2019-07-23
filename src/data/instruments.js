260396f38cf2b9b7a251156047be4e02fd3b91ab
Dylan Corvidae <dylancorvidae@gmail.com>
2019-07-23 12:07:43 -0700

    It's up and finished, yay!

--- src/data/instruments.js
+++ src/data/instruments.js
@@ -1,89 +1,102 @@
 const ibanezIceman = {
     code: 'ibanez-iceman',
     name: 'Ibanez Iceman',
-    image: 'assets/ibanez-iceman.jpg',
+    image: '../assets/ibanez-iceman.jpg',
     description: 'Totally shreds, great for downtuning',
     category: 'electric',
-    price: '800',
-    cost: '650',
+    price: 800,
+    cost: 650,
 };
 
 const fenderJazzBass = {
     code: 'fender-jazz-bass',
     name: 'Fender Jazz Bass',
-    image: 'assets/fender-jazz-bass.jpg',
+    image: '../assets/fender-jazz-bass.jpg',
     description: 'Fat tones, big low-end',
     category: 'electric',
-    price: '600',
-    cost: '450',
+    price: 600,
+    cost: 450,
 };
 const derosa12Acoustic = {
     code: 'derosa-12-acoustic',
     name: 'De Rosa 12 String Acoustic',
-    image: 'assets/derosa-12-acoustic.jpg',
+    image: '../assets/derosa-12-acoustic.jpg',
     description: 'Full acoustic sound, great for layering',
     category: 'acoustic',
-    price: '500',
-    cost: '350',
+    price: 500,
+    cost: 350,
 };
 
 const bluesDeluxe = {
     code: 'blues-deluxe',
     name: 'Fender Blue Deluxe',
-    image: 'assets/blues-deluxe.jpg',
+    image: '../assets/blues-deluxe.jpg',
     description: 'Crank it up to 11',
     category:'electric',
-    price: '500',
-    cost: '300',
+    price: 500,
+    cost: 300,
 };
 
 const yamahaCello = {
     code: 'yamaha-cello',
     name: 'Yamaha Cello',
-    image: 'assets/yamaha-cello.jpg',
+    image: '../assets/yamaha-cello.jpg',
     description: 'Bright tone, smooth action',
     category: 'acoustic',
-    price: '1200',
-    cost: '1000',
+    price: 1200,
+    cost: 1000,
 };
 
 const yamahaElectricCello = {
     code: 'yamaha-electric-cello',
     name: 'Yamaha Electric Cello',
-    image: 'assets/yamaha-electric-cello.jpg',
+    image: '../assets/yamaha-electric-cello.jpg',
     description: 'Bright tone, smooth action, looks awesome',
     category: 'electric',
-    price: '900',
-    cost: '800',
+    price: 900,
+    cost: 800,
 };
 
 const dacarboTrumpet = {
     code: 'decarbo-trumpet',
     name: 'DeCarbo Trumpet',
-    image: 'assets/decarbo-trumpet.jpg',
+    image: '../assets/decarbo-trumpet.jpg',
     description: 'Carbon fiber bell, sweet tone',
     category: 'acoustic',
-    price: '1500',
-    cost:'1100',
+    price: 1500,
+    cost: 1100,
 };
-
 
 const jupiterTenorSax = {
     code: 'jupiter-tenor-sax',
     name: 'Jupiter Tenor Saxophone',
-    image: 'assets/jupiter-tenor-sax',
+    image: '../assets/jupiter-tenor-sax.jpg',
     description: 'beautiful clear tone, silver keys',
     category: 'acoustic',
-    price: '1400',
-    cost: '1200',
+    price: 1400,
+    cost: 1200,
 };
 
 const milleniumDrumKit = {
     code: 'millenium-double-bass',
     name: 'Millenium Double Bass Drum Kit',
-    image: 'assets/millenium-double-bass.jpg',
+    image: '../assets/millenium-double-bass.jpg',
     description: 'For when you need to punish your eardrums',
     category: 'acoustic',
-    price: '2000',
-    cost: '1600',
+    price: 2000,
+    cost: 1600,
 };
+
+const instruments = [
+    ibanezIceman,
+    fenderJazzBass,
+    derosa12Acoustic,
+    bluesDeluxe,
+    yamahaCello,
+    yamahaElectricCello,
+    dacarboTrumpet,
+    jupiterTenorSax,
+    milleniumDrumKit,
+];
+
+export default instruments;
--- test/renderInstrument.test.js
+++ test/renderInstrument.test.js
@@ -9,11 +9,11 @@ test('renders an instrument', assert => {
     const ibanezIceman = {
         code: 'ibanez-iceman',
         name: 'Ibanez Iceman',
-        image: 'assets/ibanez-iceman.jpg',
+        image: '../assets/ibanez-iceman.jpg',
         description: 'Totally shreds, great for downtuning',
         category: 'electric',
-        price: '800',
-        cost: '650',
+        price: 800,
+        cost: 650,
     };
     const expected = '<li class="electric" title="Totally shreds, great for downtuning"><h3>Ibanez Iceman</h3><img src="../assets/ibanez-iceman.jpg" class="resize" alt="Ibanez Iceman image"><p class="price">$800.00<button value="ibanez-iceman">Add to cart</button></p></li>';
     //act
